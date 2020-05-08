36ca867ccb747dec35d2cd93b9f631886f259f4c
Patryk Piecuch <patrykowo@gmail.com>
2020-05-08 11:57:56 +0200

    Add bootstrap. Add new layout for navigation.

--- views/includes/head.php
+++ views/includes/head.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo (isset($this->pageTitle)) ? $this->pageTitle : APP_TITLE ?></title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/sketchy/bootstrap.min.css">
 </head>
 <body>
     
--- views/includes/navigation.php
+++ views/includes/navigation.php
@@ -1,21 +1,64 @@
-<!-- All users -->
-<a href="<?php echo APP_URL ?>">Home</a>
-<a href="<?php echo $this->url->to('posts') ?>">Posts</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="<?php $this->url->to('home') ?>">Blog MVC</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-<!-- Hide for logged -->
-<?php if(!$this->logged): ?>
-<a href="<?php echo $this->url->to('login') ?>">Login</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo APP_URL ?>">Home <span class="sr-only">(current)</span></a>
+            </li>
 
-<!-- Logged in -->
-<?php elseif($this->logged): ?>
-<!-- Admin & Owner -->
-<?php if ($this->loggedUserRole !== 'user'): ?>
-<a href="<?php echo $this->url->to('dashboard/users') ?>">Users</a>
-<a href="<?php echo $this->url->to('dashboard/create-user') ?>">Create User</a>
-<?php endif ?>
+            <!-- Hide for logged -->
+            <?php if(!$this->logged): ?>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $this->url->to('login') ?>">Login</a>
+            </li>
 
-<!-- Admin & Owner & User -->
-<a href="<?php echo $this->url->to('') ?>">Create Post</a>
-<a href="<?php echo $this->url->to('') ?>">My Posts</a>
-<a href="<?php echo $this->url->to('user/logout') ?>">Logout</a>
-<?php endif ?>
+            <!-- Logged in -->
+            <?php elseif($this->logged): ?>
+            <!-- Admin & Owner -->
+
+            <?php if ($this->loggedUserRole !== 'user'): ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Users
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="<?php echo $this->url->to('dashboard/users') ?>">All</a>
+                    <a class="dropdown-item" href="<?php echo $this->url->to('dashboard/create-user') ?>">Create</a>
+                </div>
+            </li>
+            <?php endif ?>
+
+            <!-- Admin & Owner & User -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Posts
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="<?php echo $this->url->to('') ?>">My posts</a>
+                    <a class="dropdown-item" href="<?php echo $this->url->to('') ?>">Create</a>
+                </div>
+            </li>
+
+            <!-- User profile -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Profile
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="<?php echo $this->url->to('user/edit/') . $this->userData['id'] ?>">Edit</a>
+                    <a class="dropdown-item" href="<?php echo $this->url->to('user/logout') ?>">Logout</a>
+                </div>
+            </li>
+            <?php endif ?>
+            <!-- End logged in -->
+        </ul>
+    </div>
+</nav>
