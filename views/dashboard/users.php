c66ded62366db43add440f8b26cbee038fd2b3a0
Patryk Piecuch <patrykowo@gmail.com>
2020-05-09 13:00:53 +0200

    Add bootstrap layout.

--- views/dashboard/users.php
+++ views/dashboard/users.php
@@ -1,49 +1,104 @@
-<h1>All users</h1>
+<div class="container">
+    <h1>All users</h1>
 
-<?php if (isset($this->isUserCreated)): ?>
-<p>User created successfully!</p>
-<?php endif ?>
+    <?php if (isset($this->isUserCreated)): ?>
+    <div class="row">
+        <div class="col">
+            <p class="text-success">User created successfully!</p>
+        </div>
+    </div>
+    <?php endif ?>
 
-<table>
-<thead>
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Surname</th>
-        <th>Email</th>
-        <th>Role</th>
-        <th>Created at</th>
-        <th>Actions</th>
-    </tr>
-</thead>
-<tbody>
+    <!-- Desktop & Tablet -->
+    <div class="row d-none d-md-block">
+        <div class="col">
+            <table class="table table-hover text-center">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Surname</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Created at</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-<?php foreach ($this->users as $key => $value): ?>
-    <tr>
-        <td><?php echo $value['id'] ?></td>
-        <td><?php echo $value['name'] ?></td>
-        <td><?php echo $value['surname'] ?></td>
-        <td><?php echo $value['email'] ?></td>
-        <td><?php echo $value['role'] ?></td>
-        <td><?php echo $value['created_at'] ?></td>
-        <td>
-            <!-- owner -->
-            <?php if ($this->loggedUserRole === 'owner' && $value['role'] === 'owner'): ?>
-            <a href="<?php echo $this->url->to('user/edit/') . $value['id'] ?>">Edit</a>
-            <?php endif ?>
-            <!-- admin -->
-            <?php if ($value['role'] !== 'owner'): ?>
-            <a href="<?php echo $this->url->to('user/edit/') . $value['id'] ?>">Edit</a>
-            <a href="<?php echo $this->url->to('user/delete/') . $value['id'] ?>">Delete</a>
-            <?php endif ?>
-            <?php if ($this->loggedUserRole !== 'owner' && $value['role'] === 'owner'): ?>
-            Restricted
-            <?php endif ?>
-        </td>
-    </tr>
-<?php endforeach ?>
+                    <?php foreach ($this->users as $key => $value): ?>
+                    <tr>
+                        <th scope="row"><?php echo $value['id']; ?></td>
+                        <td><?php echo $value['name']; ?></td>
+                        <td><?php echo $value['surname']; ?></td>
+                        <td><?php echo $value['email']; ?></td>
+                        <td><?php echo $value['role']; ?></td>
+                        <td><?php echo $value['created_at']; ?></td>
+                        <td>
+                            <!-- owner -->
+                            <?php if ($this->loggedUserRole === 'owner' && $value['role'] === 'owner'): ?>
+                            <a href="<?php echo $this->url->to('user/edit/') . $value['id']; ?>" class="btn btn-sm btn-info">Edit</a>
+                            <?php endif ?>
+                            <!-- admin -->
+                            <?php if ($value['role'] !== 'owner'): ?>
+                            <a href="<?php echo $this->url->to('user/edit/') . $value['id']; ?>" class="btn btn-sm btn-info">Edit</a>
+                            <a href="<?php echo $this->url->to('user/delete/') . $value['id']; ?>" class="btn btn-sm btn-danger">Delete</a>
+                            <?php endif ?>
+                            <?php if ($this->loggedUserRole !== 'owner' && $value['role'] === 'owner'): ?>
+                            <span class="text-muted">Restricted</span>
+                            <?php endif ?>
+                        </td>
+                    </tr>
+                    <?php endforeach ?>
 
-</tbody>
-</table>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <!-- Mobile only -->
+    <div class="row d-block d-md-none">
+        <div class="col">
+            <ul class="list-group">
+                <?php foreach ($this->users as $key => $value): ?>
+                <li class="list-group-item">
+                    <div>
+                        ID: <?php echo $value['id']; ?>
+                    </div>
+                    <div>
+                        Name: <?php echo $value['name']; ?>
+                    </div>
+                    <div>
+                        Surname: <?php echo $value['surname']; ?>
+                    </div>
+                    <div>
+                        Email: <?php echo $value['email']; ?>
+                    </div>
+                    <div>
+                        Role: <?php echo $value['role']; ?>
+                    </div>
+                    <div>
+                        Created at: <?php echo $value['created_at']; ?>
+                    </div>
+                    <div class="d-flex justify-content-around p-3">
+                        <!-- owner -->
+                        <?php if ($this->loggedUserRole === 'owner' && $value['role'] === 'owner'): ?>
+                        <a href="<?php echo $this->url->to('user/edit/') . $value['id']; ?>" class="btn btn-sm btn-info">Edit</a>
+                        <?php endif ?>
+                        <!-- admin -->
+                        <?php if ($value['role'] !== 'owner'): ?>
+                        <a href="<?php echo $this->url->to('user/edit/') . $value['id']; ?>" class="btn btn-sm btn-info">Edit</a>
+                        <a href="<?php echo $this->url->to('user/delete/') . $value['id']; ?>" class="btn btn-sm btn-danger">Delete</a>
+                        <?php endif ?>
+                        <?php if ($this->loggedUserRole !== 'owner' && $value['role'] === 'owner'): ?>
+                        <span class="text-muted">Actions not permitted</span>
+                        <?php endif ?>
+                    </div>
+                </li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+    </div>
+</div>
 
 
