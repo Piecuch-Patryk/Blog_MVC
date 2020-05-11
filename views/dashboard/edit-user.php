feec65480be71704a58c67f6f7dbc51b9bfb0dff
Patryk Piecuch <patrykowo@gmail.com>
2020-05-11 12:16:15 +0200

    Add bootstrap layout.

--- views/dashboard/edit-user.php
+++ views/dashboard/edit-user.php
@@ -1,24 +1,40 @@
-<h1>Edit user - <?php echo $this->fullName; ?></h1>
-
-<form action="<?php echo $this->url->to('user/update'); ?>" method="post">
-    <label for="name">Name</label>
-    <input name="name" type="text" value="<?php echo $this->name; ?>">
-
-    <label for="surname">Surname</label>
-    <input name="surname" type="text" value="<?php echo $this->surname; ?>">
-
-    <label for="email">Email</label>
-    <input name="email" type="text" value="<?php echo $this->email; ?>" disabled>
-
-    <label for="role">Role</label>
-    <select name="role">
-        <option value="user" <?php echo $this->role === 'user' ? 'selected' : ''; ?>>User</option>
-        <option value="admin" <?php echo $this->role === 'admin' ? 'selected' : ''; ?>>Admin</option>
-        <option value="owner" <?php echo $this->role === 'owner' ? 'selected ' : ''; echo $this->loggedUserRole !== 'owner' ? 'disabled' : ''; ?>>Owner</option>
-    </select>
-
-    <label for="email">Created at</label>
-    <input name="email" type="text" value="<?php echo $this->created_at; ?>" disabled>
-
-    <button>Submit</button>
-</form>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Edit user - <?php echo $this->fullName; ?></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-10 mx-auto">
+            <form action="<?php echo $this->url->to('user/update') ?>" method="post">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input name="name" type="text" value="<?php echo $this->name; ?>" class="form-control" aria-describedby="name">
+                </div>
+                <div class="form-group">
+                    <label for="surname">Surname</label>
+                    <input name="surname" type="text" value="<?php echo $this->surname; ?>" class="form-control" aria-describedby="surname">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input name="email" type="text" value="<?php echo $this->email; ?>" class="form-control text-muted" disabled aria-describedby="surname">
+                </div>
+                <div class="form-group">
+                    <label for="role">Role</label>
+                    <select name="role" class="form-control">
+                        <option value="user" <?php echo $this->role === 'user' ? 'selected' : ''; ?>>User</option>
+                        <option value="admin" <?php echo $this->role === 'admin' ? 'selected' : ''; ?>>Admin</option>
+                        <option value="owner" <?php echo $this->role === 'owner' ? 'selected ' : ''; echo $this->loggedUserRole !== 'owner' ? 'disabled' : ''; ?>>Owner</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="created_at">Created at</label>
+                    <input name="created_at" type="text" value="<?php echo $this->created_at; ?>" class="form-control text-muted" disabled aria-describedby="created at">
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
