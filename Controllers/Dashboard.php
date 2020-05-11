3385be9ed873d40598973f012b6d53838225f1a8
Patryk Piecuch <patrykowo@gmail.com>
2020-05-11 10:47:13 +0200

    Add bootstrap layout.

--- Controllers/Dashboard.php
+++ Controllers/Dashboard.php
@@ -18,6 +18,13 @@ class Dashboard extends Controller
         parent::__construct();
         $this->view->userData = $_SESSION;
         $this->view->logged = true;
+        $this->view->postedData = '';
+        $this->view->e_name = '';
+        $this->view->e_surname = '';
+        $this->view->e_email = '';
+        $this->view->e_password = '';
+        $this->view->e_password_repeat = '';
+        $this->view->e_logging = '';
     }
         
     /**
--- views/dashboard/create-user.php
+++ views/dashboard/create-user.php
@@ -21,27 +21,50 @@
 </ul>
 <?php endif ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col-10 mx-auto">
+            
+            <form action="<?php echo $this->url->to('user/store') ?>" method="post">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input name="name" type="text" value="<?php echo isset($this->postedData['name']) ? htmlentities($this->postedData['name']) : ''; ?>" class="form-control" aria-describedby="name">
+                    <p class="text-danger"><?php echo $this->e_name; ?></p>
+                </div>
+                <div class="form-group">
+                    <label for="surname">Surname</label>
+                    <input name="surname" type="text" value="<?php echo isset($this->postedData['surname']) ? htmlentities($this->postedData['surname']) : ''; ?>" class="form-control" aria-describedby="surname">
+                    <p class="text-danger"><?php echo $this->e_surname; ?></p>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input name="email" type="text" value="<?php echo isset($this->postedData['email']) ? htmlentities($this->postedData['email']) : ''; ?>" class="form-control" aria-describedby="email">
+                    <p class="text-danger"><?php echo $this->e_email; ?></p>
+                </div>
+                <div class="form-group">
+                    <label for="role">Role</label>
+                    <select name="role" class="form-control" aria-describedby="role">
+                        <?php isset($this->postedData['role']) ? $this->role = $this->postedData['role'] : $this->role = ''; ?>
+                        <option value="user" <?php echo $this->role === 'user' ? 'selected' : ''; ?>>User</option>
+                        <option value="admin" <?php echo $this->role === 'admin' ? 'selected' : ''; ?>>Admin</option>
+                        <option value="owner" <?php echo $this->role === 'owner' ? 'selected' : ''; ?>>Owner</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input name="password" type="password" class="form-control" aria-describedby="password">
+                    <p class="text-danger"><?php echo $this->e_password; ?></p>
+                </div>
+                <div class="form-group">
+                    <label for="password-repeat">Password Repeat</label>
+                    <input name="password-repeat" type="password" class="form-control" aria-describedby="password repeat">
+                    <p class="text-danger"><?php echo $this->e_password_repeat; ?></p>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
 
-<form action="<?php echo $this->url->to('user/store') ?>" method="post">
-
-    <label for="name">Name</label>
-    <input name="name" type="text" value="<?php echo isset($this->postedData['name']) ? $this->postedData['name'] : ''; ?>">
-    <label for="surname">Surname</label>
-    <input name="surname" type="text" value="<?php echo isset($this->postedData['surname']) ? $this->postedData['surname'] : ''; ?>">
-    <label for="email">Email</label>
-    <input name="email" type="text" value="<?php echo isset($this->postedData['email']) ? $this->postedData['email'] : ''; ?>">
-    <label for="role">Role</label>
-    <select name="role">
-        <?php isset($this->postedData['role']) ? $this->role = $this->postedData['role'] : $this->role = ''; ?>
-        <option value="user" <?php echo $this->role === 'user' ? 'selected' : ''; ?>>User</option>
-        <option value="admin" <?php echo $this->role === 'admin' ? 'selected' : ''; ?>>Admin</option>
-        <option value="owner" <?php echo $this->role === 'owner' ? 'selected' : ''; ?>>Owner</option>
-    </select>
-    <label for="password">Password</label>
-    <input name="password" type="password">
-    <label for="password-repeat">Password Repeat</label>
-    <input name="password-repeat" type="password">
-
-    <button>Submit</button>
-
-</form>
+        </div>
+    </div>
+</div>
